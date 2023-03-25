a7ed5d2e6a94ce9f3a473997b06cbe798a968dad
Roman Travnikov <roman@travnikov.dev>
2023-03-25 12:54:06 +0300

    Init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as ffmpeg from 'fluent-ffmpeg';
+import * as ffmpegStatic from 'ffmpeg-static';
+import * as ffprobeStatic from 'ffprobe-static';
+import * as sharp from 'sharp';
+
+// Your project code goes here
